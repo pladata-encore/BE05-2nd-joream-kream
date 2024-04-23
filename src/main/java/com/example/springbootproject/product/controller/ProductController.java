9e10eda90339f745402071f00f71e136da819e53
Kim JaeMin <2098kjm@naver.com>
2024-04-23 19:28:54 +0900

    [Feat] Products 전체 조회, 상세 조회

--- src/main/java/com/example/springbootproject/product/controller/ProductController.java
+++ src/main/java/com/example/springbootproject/product/controller/ProductController.java
@@ -1,11 +1,31 @@
 package com.example.springbootproject.product.controller;
 
+import com.example.springbootproject.product.domain.Product;
+import com.example.springbootproject.product.dto.response.ProductResponse;
+import com.example.springbootproject.product.service.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/products")
 @AllArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
+    // 전체 조회
+    @GetMapping
+    public List<ProductResponse> getAllProducts() {
+        return productService.findAll();
+    }
+
+    // 상세 조회
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") Long id) {
+        return productService.findById(id);
+    }
 }
--- src/main/java/com/example/springbootproject/product/service/ProductService.java
+++ src/main/java/com/example/springbootproject/product/service/ProductService.java
@@ -1,4 +1,11 @@
 package com.example.springbootproject.product.service;
 
+import com.example.springbootproject.product.domain.Product;
+import com.example.springbootproject.product.dto.response.ProductResponse;
+
+import java.util.List;
+
 public interface ProductService {
+    List<ProductResponse> findAll();
+    Product findById(Long id);
 }
