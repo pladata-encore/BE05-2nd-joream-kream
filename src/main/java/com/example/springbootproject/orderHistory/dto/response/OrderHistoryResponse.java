e738575c23f055dd465aa4b0c05b3db00b62baae
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-04-25 12:42:21 +0900

    [Feat]: OrderHistory에서 판매 혹은 구매에 맞는 거래내역 가져오기 기능 개발 및 전역예외 처리 다른 예외 추가

--- src/main/java/com/example/springbootproject/orderHistory/dto/response/OrderHistoryResponse.java
+++ src/main/java/com/example/springbootproject/orderHistory/dto/response/OrderHistoryResponse.java
@@ -1,4 +1,23 @@
 package com.example.springbootproject.orderHistory.dto.response;
 
-public record OrderHistoryResponse() {
+import com.example.springbootproject.orderHistory.domain.OrderHistory;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record OrderHistoryResponse(String productName, Long productPrice , String productSize, boolean isSell, LocalDateTime createdAt) {
+
+
+
+
+    public static OrderHistoryResponse from(OrderHistory orderHistory, Long myId){
+        return OrderHistoryResponse.builder()
+                .productName(orderHistory.getProductName())
+                .productPrice(orderHistory.getProductPrice())
+                .productSize(orderHistory.getProductSize())
+                .createdAt(orderHistory.getCreatedAt())
+                .isSell(orderHistory.getSellerId().equals(myId))
+                .build();
+    }
 }
