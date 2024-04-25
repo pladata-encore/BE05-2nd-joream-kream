e7013622c5378297ee63dd1867bbfee76b85fd12
jungryuel <zldrktduf0927@naver.com>
2024-04-25 23:44:55 +0900

    [Feat]: N+1 발생 부분 쿼리 수정 및 엔티티에 조인 패치 추가

--- src/main/java/com/example/springbootproject/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/example/springbootproject/wishlist/repository/WishlistRepository.java
@@ -1,16 +1,19 @@
 package com.example.springbootproject.wishlist.repository;
 
 import com.example.springbootproject.auth.domain.User;
-import com.example.springbootproject.product.domain.Product;
 import com.example.springbootproject.size.domain.Size;
 import com.example.springbootproject.wishlist.domain.Wishlist;
-import com.example.springbootproject.wishlist.dto.response.WishlistResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface WishlistRepository extends JpaRepository<Wishlist,Long> {
+    @Query("select distinct w from Wishlist w " +
+            "join fetch w.user u " +
+            "join fetch w.size s " +
+            "where u.id = :userId")
     List<Wishlist> findAllByUser_Id(Long userId);
     Optional<Wishlist> findByUserAndSize(User user, Size size);
 }
