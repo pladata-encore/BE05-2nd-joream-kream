1e9549c30080319cf5466b79b3d57ca750fd9df4
jungryuel <zldrktduf0927@naver.com>
2024-04-25 17:25:04 +0900

    [Feat]: N+1 발생 부분 쿼리 수정 및 엔티티에 조인 패치 추가

--- src/main/java/com/example/springbootproject/product/repository/ProductRepository.java
+++ src/main/java/com/example/springbootproject/product/repository/ProductRepository.java
@@ -1,13 +1,24 @@
 package com.example.springbootproject.product.repository;
 
-import com.example.springbootproject.brand.dto.response.BrandResponse;
 import com.example.springbootproject.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
     List<Product> findAllByBrand_Id(Long brandId);
+
+    //엔티티의 fetch가 LAZY여야 할 때 쿼리메서드를 직접 만들어 리턴
+    @Query("select p from Product p " +
+            "join fetch p.brand b where p.id = :Id")
+    Optional<Product> findById(Long Id);
+    @Query("select p from Product p " +
+            "join fetch p.brand b where p.name like concat('%', :productName, '%') ")
     List<Product> findByNameContaining(String productName);
+
+    @Query("select p from Product p " +
+            "join fetch p.brand b where p.category like concat('%', :category, '%') ")
     List<Product> findByCategoryContaining(String category);
 }
