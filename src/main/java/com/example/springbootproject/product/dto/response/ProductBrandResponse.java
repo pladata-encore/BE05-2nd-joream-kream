ebe7b40fd920dd4182b47caf40ea0a84fb1f1439
jungryuel <zldrktduf0927@naver.com>
2024-04-26 15:38:13 +0900

    변경사항 추가

--- src/main/java/com/example/springbootproject/product/dto/response/ProductBrandResponse.java
+++ src/main/java/com/example/springbootproject/product/dto/response/ProductBrandResponse.java
@@ -4,11 +4,12 @@ import com.example.springbootproject.product.domain.Product;
 
 import java.util.Date;
 
-public record ProductBrandResponse(String productName, String category, Integer release_price, String modelCode, String productColor, Date releasedAt) {
+public record ProductBrandResponse(String productName, String brandName, String category, Integer release_price, String modelCode, String productColor, Date releasedAt) {
     public static ProductBrandResponse from(Product product) {
         ProductBrandResponse productBrand =
                 new ProductBrandResponse(
                         product.getName(),
+                        product.getBrand().getBrandName(),
                         product.getCategory(),
                         product.getReleasePrice(),
                         product.getModelCode(),
